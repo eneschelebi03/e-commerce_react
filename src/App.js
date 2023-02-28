81e2f75f7014218d4aa70e8dd613986b402d27c2
ShadowCode887 <liontiger851@gmail.com>
2023-02-28 17:57:29 +0200

    Offer slider and Flash Products slider added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
 import Header from "./common/header/Header";
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Pages from "./pages/Pages";
+import Data from "./components/flashDeals/Data";
 
 function App() {
+  // todo: fetch from database
+  const { productItems } = Data
+  
+  const [cartItem, setCartItem] = useState([])
+ 
   return (
     <>
       <Router>
         <Header />
         <Switch>
-          {/* <Route path="/" exact>
-            <About />
-          </Route> */}
+          <Route path="/" exact>
+            <Pages productItems={productItems} />
+          </Route>
         </Switch>
       </Router>
     </>
