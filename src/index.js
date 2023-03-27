bdfefd557b7cb7f4d91932577f49b15c6c2952ed
ShadowCode887 <liontiger851@gmail.com>
2023-03-27 17:29:57 +0300

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './App';
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
